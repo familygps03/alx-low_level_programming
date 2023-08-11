7896137ff2c09885da0e47350820e233b5d84193
familygps03 <gmozey02@gmail.com>
2023-08-10 18:04:15 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: this is  always zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
