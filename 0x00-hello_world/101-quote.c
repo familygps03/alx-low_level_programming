5bc21b4cd607756320a636ad0b02225f092ab3eb
familygps03 <gmozey02@gmail.com>
2023-08-11 18:49:53 -0700

    c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - it is the main function of the program 
+ * main -it is the main function of the program 
  * Return 1 (right)
  */
 int main(void)
-{	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+{	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+readme file for variables,if,while loops
