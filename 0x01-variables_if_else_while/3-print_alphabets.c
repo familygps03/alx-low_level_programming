45e3e7335430d05d0b549e982e65983d5e278fd1
familygps03 <gmozey02@gmail.com>
2023-08-12 12:41:50 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 	char p;
 
 	for (p = 'a'; p <= 'z'; p++)
-		putchar('p');
+		putchar(p);
 	for (p = 'A'; p <= 'Z';  p++)
-		putchar('p');
+		putchar(p);
 	putchar('\n');
 	return (0);
 }
