effc9703fefa46341fd519b7835480a9b2d975d3
familygps03 <gmozey02@gmail.com>
2023-08-11 20:14:56 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,12 +18,21 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	p = n % 10;
-	if (p > 5)
-		printf("Last digit of %d is %d and is greater than 5", n, p);
-	else if (p == 0)
-		printf("last digit of %d is %d and is 0", n, p);
-	else if (p < 6)
-		printf("Last digit of %d is %d and is less that 6 and not 0", n, p);
+	if (p == 0)
+	{	
+		printf("Last digit of %d is and is 0", n, p);
+	}
+	else		
+	{	
+		if (p > 5)
+		{
+		printf("last digit of %d is %d and is greater than 5", n, p);
+		}
+		else if (p < 6)
+		{
+			printf("Last digit of %d is %d and is less that 6 and not 0", n, p);
+		}
+	}
 	printf("\n");
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is always main function
+ * Return: It is always zero
+ */
+
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
