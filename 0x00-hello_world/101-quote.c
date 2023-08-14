2e4392c59fb80e9af12ca5ffce980f3c4d0c2f14
familygps03 <gmozey02@gmail.com>
2023-08-14 15:02:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - main function in the task
-*
-*
-*Return: return 1
+* main - This is the main function
+*Return: 1 (success)
 */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
