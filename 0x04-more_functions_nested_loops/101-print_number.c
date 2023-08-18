32a4de27f4fd538d3ac6a961aeaf2a74c99472ba
familygps03 <gmozey02@gmail.com>
2023-08-17 19:35:27 -0700

    main.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_number - Prints a digitt
+* @n: The number to print
+*
+*/
+void print_number(int n)
+{
+unsigned int digit = n;
+if (n < 0)
+{
+_putchar('-');
+digit = -digit;
+}
+if (digit > 9)
+{
+print_number(digit / 10);
+}
+_putchar(digit % 10 + '0');
+}
