99ec4173e2dba1bad782dea043aa3fa8669fbe3a
familygps03 <gmozey02@gmail.com>
2023-08-18 12:49:06 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns 0
+*/
+void print_numbers(void)
+{
+int number = 0;
+for (number = 0; number < 9; number++)
+{
+_putchar(number);
+}
+_putchar('\n');
+}
