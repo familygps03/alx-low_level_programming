aed9b8b701478531ac53a2cc69d6a9f492a34a1d
familygps03 <gmozey02@gmail.com>
2023-08-18 09:39:58 -0700

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,12 @@
 void print_most_numbers(void)
 {
 int num;
-for (num = 48; num < 58; num++)
+for (num = 0; num <= 9; num++)
 {
-if ((num == 50) || (num == 52))
+if ((num != 2) && (num != 4))
 {
-continue;
-}
-_putchar(num);
+_putchar(num + '0');
 }
 _putchar('\n');
+}
 }
