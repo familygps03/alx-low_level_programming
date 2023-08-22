b2d0269f238eb5906dd131c46a276c7c78bab640
familygps03 <gmozey02@gmail.com>
2023-08-21 18:37:34 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point generates a random password
+ *
+ * Return: the generated password
+ */
+int main(void)
+{
+char c;
+int y;
+
+srand(time(0));
+while (y <= 1500)
+{
+c = rand() % 128;
+y += c;
+putchar(c);
+}
+putchar(2000 - y);
+return (0);
+}
