815f43dd07c279d1e33e8b859b9fdd1438c31387
familygps03 <gmozey02@gmail.com>
2023-08-21 19:19:04 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,12 +12,12 @@ char c;
 int y;
 
 srand(time(0));
-while (y <= 2645)
+while (y <= 1500)
 {
-c = ((rand() % 128) != '\0');
+c = rand() % 128;
 y += c;
 putchar(c);
 }
-putchar(2772 - y);
+putchar(2000 - y);
 return (0);
 }
