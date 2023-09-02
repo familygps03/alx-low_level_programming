14157544c4b34728b63bc9347ca80f9391c9d8ff
familygps03 <gmozey02@gmail.com>
2023-09-01 18:22:43 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints all arguments it received
+*@argc: argument count
+*@argv: argument vector
+*Return: returns 0
+*/
+
+int main(int argc, char *argv[])
+{
+		int a;
+
+		for (a = 0; a < argc; a++)
+			printf("%s\n", argv[a]);
+
+		return (0);
+}
