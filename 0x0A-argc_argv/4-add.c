fff1a6e0ad961edbf76f582c7234d856cf3d25bb
familygps03 <gmozey02@gmail.com>
2023-09-01 18:29:32 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - adds positive numbers
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0 on success, 1 on failure
+*/
+
+int main(int argc, char *argv[])
+{
+	int a, b, sum = 0;
+
+	for (a = 1; b < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[a]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
