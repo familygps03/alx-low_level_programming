a1410a5f4d192f61ecd9c930c9593ef853240ab1
familygps03 <gmozey02@gmail.com>
2023-09-07 18:55:21 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,4 +8,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 #endif    
