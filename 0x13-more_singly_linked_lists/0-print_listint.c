ed5e827750d40db5665edd5917f6b0f0a2797573
familygps03 <gmozey02@gmail.com>
2023-09-27 17:23:16 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,11 +14,8 @@ size_t print_listint(const listint_t *h)
 
 	size_t count = 0;
 
-	if (h == NULL)
-		return (0);
-	printf("%d\n", h->n);
-	count++;
-	while (h->next)
+
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
