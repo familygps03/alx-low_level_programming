15dc607a63ce2f13f3fd8953e35fffe9c9106c31
familygps03 <gmozey02@gmail.com>
2023-10-06 19:31:11 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,75 +6,84 @@
 void close_file_descriptor(int fd);
 
 /**
-* allocate_buffer - this is the function that allocates memory for
+* allocate_memory - this is the function that allocates memory for
 * buffer and returns a pointer to it.
-*@file_name: this is the name the file that is processed.
-*@buffer_size: this is the size of the buffer
+*@dest_file: this is the destination file where data will be written.
 * Return: this returns the pointer to the newly-allocated buffer.
 */
 
 
-char *allocate_buffer(const char *file_name, size_t buffer_size)
+char *allocate_memory(const char *dest_file)
 {
-	int file_desc;
-	char *buffer;
+	char *data_buffer;
 
-	file_desc = open(file_name, O_RDONLY);
+	data_buffer = malloc(sizeof(char) * 1024);
 
-	if (file_desc == -1)
+	if (!data_buffer)
 	{
-	dprintf(STDERR_FILENO, "Error: can not open from file to %s\n", file_name);
+	dprintf(STDERR_FILENO, "Error: can not allocate memory for %s\n", dest_file);
 	exit(EXIT_FAILURE);
 	}
-	buffer = malloc(buffer_size);
-
-	if (buffer == NULL)
-	{
-
-		dprintf(STDERR_FILENO, "Error: can not malloc\n");
-		close_file_descriptor(file_desc);
-		exit(EXIT_FAILURE);
-	}
-	
-	return buffer;
+	return (data_buffer);
 }
 
 /**
  * close_file_descriptor - this is the function that closes a file
  * descriptor and checks for errors.
  *@fd: this is the file descriptor to be closed.
- *@file_name: this is the name of the file being processed
+ * Return: this returns no value
  */
 
 void close_file_descriptor(int fd)
 {
-	if (close(fd) == -1)
+	int close_status;
+
+	close_status = close(fd);
+
+	if (close_status == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can not close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: can not close file descriptor %d\n", fd);
 		exit(EXIT_FAILURE);
 	}
 }
 
 /**
 * main - this is the function that copies the content of the
-* file to the another file
+* source file to the destination file
 *@argv: this is array of the arguments command-line
 *@argc: this is the number of the arguments command-line
-*Return: this returns zero when success
+*Return: this returns zero when success or appropriate error
+* codes on failure
 */
 
 int main(int argc, char *argv[])
 {
+	int source_fd, dest_fd, bytes_read;
+	char *data_buffer;
 
-	if (argc != 2)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s filename\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp source_file dest_file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	allocate_buffer(argv[1], 1024);
+	data_buffer = allocate_memory(argv[2]);
+	source_fd = open(argv[1], O_RDONLY);
+	bytes_read = read(source_fd, data_buffer, 1024);
+	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	close_file_descriptor(0);
-
-	return EXIT_SUCCESS;
+	do {
+		if (source_fd == -1 || bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can not read from file %s\n", argv[1]);
+			free(data_buffer);
+			exit(EXIT_FAILURE);
+		}
+		bytes_read = read(source_fd, data_buffer, 1024);
+		dest_fd = open(argv[2], O_WRONLY | O_APPEND);
+	} while (bytes_read > 0);
+	free(data_buffer);
+	close_file_descriptor(source_fd);
+	close_file_descriptor(dest_fd);
+	return (EXIT_SUCCESS);
 }
