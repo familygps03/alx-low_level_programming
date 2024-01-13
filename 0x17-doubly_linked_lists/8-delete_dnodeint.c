611fc74076037cace6b050ad00a4669e02281ccd
familygps03 <gmozey02@gmail.com>
2024-01-13 01:27:02 +0000

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,49 +1,36 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - this function deletes the node at
- * index index of a dlistint_t linked list
- *
- * @head: this is the variable of first element
- * @index: elements number
- *
- * Return: it returns error code
- *
- **/
-
+ * delete_dnodeint_at_index - this is the function that deletes
+ * the node at index od a DLL
+ * @head: this is the  double pointer to the head of the list
+ * @index: this is  the node that should be delete
+ * Return: it returns 1 if it succeeded or -1 if it failed
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *before, *current;
-	unsigned int m = 0;
+	dlistint_t *aux_head = *head;
 
-	current = *head;
-
-	if (current == NULL)
+	if (*head == NULL)
 		return (-1);
-
-	if (index == 0)
+	while (index > 0)
 	{
-		*head = current->next;
-		current->prev = NULL;
-		free(current);
-
-		return (1);
-	}
-
-	while (m < index)
-	{
-		before = current;
-		if (current->next == NULL)
+		if (aux_head == NULL)
 			return (-1);
-
-		current = current->next;
-		m++;
+		aux_head = aux_head->next;
+		index--;
 	}
-	before->next = current->next;
-
-	if (current->next != NULL)
-		current->next->prev = before;
-	free(current);
-
+	if (aux_head == *head)
+	{
+		*head = aux_head->next;
+		(*head)->prev = NULL;
+	}
+	else
+	{
+		aux_head->prev->next = aux_head->next;
+		if (aux_head->next)
+			aux_head->next->prev = aux_head->prev;
+	}
+	free(aux_head);
 	return (1);
 }
