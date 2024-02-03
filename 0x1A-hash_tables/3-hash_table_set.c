cc2d0f4babdc674c3fd3160700f6f9071779bafe
familygps03 <gmozey02@gmail.com>
2024-02-03 01:12:22 +0000

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,17 +1,15 @@
 #include "hash_tables.h"
 
-hash_node_t *create_node(const char *key, const char *value);
-
 /**
- * hash_table_set - Add or update an element in the hash table.
- * @ht: Pointer to the hash table to add or update the key/value.
- * @key: Pointer to the key; cannot be an empty string.
- * @value: Pointer to the value associated with the key.
+ * hash_table_set -  function that adds an element to the hash table.
+ * @ht: pointer to hash table you want to add or update the key/value to.
+ * @key: pointer to key and cannot be an empty string.
+ * @value: pointer to the value is the value associated with the key.
  *
- * The value must be duplicated, and it can be an empty string.
- * In case of a collision, add the new_node node at the beginning of the list.
+ * value must be duplicated. value can be an empty string.
+ * In case of collision, add the new_node node at the beginning of the list.
  *
- * Return: 1 if successful, 0 otherwise.
+ * Return: 1 if it succeeded, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -20,45 +18,45 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int i;
 
 	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
-	return (0);
+		return (0);
 	i = key_index((unsigned char *)key, ht->size);
 	current_node = ht->array[i];
 
 	for (; current_node != NULL; current_node = current_node->next)
 	{
-	if (strcmp(current_node->key, key) == 0)
-	{
-		if (strcmp(current_node->value, value) != 0)
-	{
-		free(current_node->value);
-		current_node->value = strdup(value);
-	}
+		if (strcmp(current_node->key, key) == 0)
+		{
+			if (strcmp(current_node->value, value) != 0)
+			{
+				free(current_node->value);
+				current_node->value = strdup(value);
+			}
 		return (1);
-	}
+		}
 	}
 
-	new_node = create_node(key, value);
+	new_node = _node_(key, value);
 	if (new_node == NULL)
-	return (0);
-
+		return (0);
 	new_node->next = ht->array[i];
 	ht->array[i] = new_node;
 	return (1);
 }
 
+
 /**
- * create_node - Create a new_node node.
- * @key: Pointer to the key.
- * @value: Pointer to the value associated with the key.
+ * _node_ - function that creates a new_node node.
+ * @key: pointer to key.
+ * @value: pointer to value of the key.
  *
- * Return: Pointer to the new_node node or NULL on failure.
+ * Return: pointer to new_node node else NULL.
  */
-hash_node_t *create_node(const char *key, const char *value)
+hash_node_t *_node_(const char *key, const char *value)
 {
 	hash_node_t *new_node;
 
 	if (key == NULL || strcmp(key, "") == 0 || value == NULL)
-	return (NULL);
+		return (NULL);
 
 	new_node = malloc(sizeof(hash_node_t));
 
@@ -69,9 +67,10 @@ hash_node_t *create_node(const char *key, const char *value)
 
 	if (new_node->key == NULL)
 	{
-	free(new_node);
-	return (NULL);
+		free(new_node);
+		return (NULL);
 	}
+
 	new_node->value = strdup(value);
 
 	if (new_node->value == NULL)
@@ -86,7 +85,7 @@ hash_node_t *create_node(const char *key, const char *value)
 }
 
 /**
- * Note: If you want to test for collisions, here are some strings
+ * description - If you want to test for collisions, here are some strings,
  * that collide using the djb2 algorithm:
  *
  * hetairas collides with mentioner
